7722f2069716288f8ba3a265ea880a1f220f4141
GG <zjjhgzh@gmail.com>
2023-04-14 23:38:28 +0300

    update

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -104,7 +104,6 @@ int main(int argc, char *argv[])
 
   gettimeofday(&T2, NULL); /* запомнить текущее время T2 */
   delta_ms = 1000 * (T2.tv_sec - T1.tv_sec) + (T2.tv_usec - T1.tv_usec) / 1000;
-  delta_ms +=1;
   printf("\n N=%d. Milliseconds passed: %ld\n", N, delta_ms); /* T2 - T1 */
   //printf("X: %fd\n", X); /* T2 -T1 */
   return 0;
