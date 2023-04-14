664730415b89253dc8ae6f293fd3f10707ec1fa1
GG <zjjhgzh@gmail.com>
2023-04-14 20:55:28 +0300

    update

--- lab1/build_clang.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-clang -O3 -Wall -Werror -o lab1-seq-clang lab1.c -lm
-clang -O3 -Wall -Werror -mllvm -polly -mllvm -polly-parallel -lgomp -mllvm -polly-num-threads=1 -o lab1-par-clang-1 lab1.c -lm
-clang -O3 -Wall -Werror -mllvm -polly -mllvm -polly-parallel -lgomp -mllvm -polly-num-threads=2 -o lab1-par-clang-2 lab1.c -lm
-clang -O3 -Wall -Werror -mllvm -polly -mllvm -polly-parallel -lgomp -mllvm -polly-num-threads=4 -o lab1-par-clang-4 lab1.c -lm
-clang -O3 -Wall -Werror -mllvm -polly -mllvm -polly-parallel -lgomp -mllvm -polly-num-threads=8 -o lab1-par-clang-8 lab1.c -lm
---
+++ lab2/build.sh
@@ -0,0 +1,1 @@
+gcc -O3 -std=c99 -Wall -Werror -o lab2-fw lab2.c -lm -I ./FW_1.3.1_Lin64 -L ./FW_1.3.1_Lin64/lib/ -lfwImage -lfwBase -lfwSignal
