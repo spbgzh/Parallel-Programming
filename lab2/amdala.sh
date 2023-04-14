5de1ccfceefe2a0154cd40b8af9e075946004745
GG <zjjhgzh@gmail.com>
2023-04-15 00:15:33 +0300

    update

--- lab2/amdala.sh
+++ lab2/amdala.sh
@@ -1,13 +1,21 @@
-delta=100
-N1=100
-N2=1100
+N=12345
+
 export LD_LIBRARY_PATH=./FW_1.3.1_Lin64/lib
-for ((var = $N1; var <= $N2; var += $delta)); do
-    echo $var >> amdala_fw.txt
-    ./lab2-fw $var 1 >> amdala_fw.txt
-    ./lab2-fw $var 2 >> amdala_fw.txt
-    ./lab2-fw $var 4 >> amdala_fw.txt
-    ./lab2-fw $var 8 >> amdala_fw.txt
-    ./lab2-fw $var 16 >> amdala_fw.txt
-    ./lab2-fw $var 32 >> amdala_fw.txt
+for ((var = 0; var <= 10; var += 1)); do
+    ./lab2-fw $N 1 >> amdala_fw.txt
+done
+for ((var = 0; var <= 10; var += 1)); do
+    ./lab2-fw $N 2 >> amdala_fw.txt
+done
+for ((var = 0; var <= 10; var += 1)); do
+    ./lab2-fw $N 4 >> amdala_fw.txt
+done
+for ((var = 0; var <= 10; var += 1)); do
+    ./lab2-fw $N 8 >> amdala_fw.txt
+done
+for ((var = 0; var <= 10; var += 1)); do
+    ./lab2-fw $N 16 >> amdala_fw.txt
+done
+for ((var = 0; var <= 10; var += 1)); do
+    ./lab2-fw $N 32 >> amdala_fw.txt
 done
