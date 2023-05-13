fbfc99943e749d4e2406491cc634117fbaa617b8
GG <zjjhgzh@gmail.com>
2023-05-13 09:00:56 +0300

    update

--- lab3/test.sh
+++ lab3/test.sh
@@ -14,9 +14,3 @@ done
 for ((var = $N1; var <= $N2; var += $delta)); do
     ./lab3 $var 8 >> lab3_8.txt
 done
-for ((var = $N1; var <= $N2; var += $delta)); do
-    ./lab3 $var 16 >> lab3_16.txt
-done
-for ((var = $N1; var <= $N2; var += $delta)); do
-    ./lab3 $var 32 >> lab3_32.txt
-done
