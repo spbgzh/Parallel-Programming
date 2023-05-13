50e34248716d808f015009334965984897c479b3
GG <zjjhgzh@gmail.com>
2023-05-13 11:37:23 +0300

    update data

--- lab3/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc -std=c99 -O3 -Wall -Werror -o lab3 lab3.c -lm -fopenmp
-gcc -std=c99 -O3 -o lab3_without_fopenmp lab3.c -lm 
---
+++ lab3/data/a.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+path = "./lab3/data/"
+list_ = []
+for index in ["1", "2", "4", "8"]:
+    with open(path+"lab3_static8_"+index+".txt", "r") as f:  # 打开文件
+        data = f.readlines()  # 读取文件
+        for i in data:
+            if i.startswith(" N="):
+
+                list_.append(i.split("passed: ")[-1].split("\n")[0])
+
+for i in range(11):
+    temp = ""
+    for j in range(4):
+        temp = temp+str(list_[11*j+i])+','
+    print(temp)
